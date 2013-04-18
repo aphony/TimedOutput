db4d6aa98a75651fa92afd9ad45395823839ebed
aphonydissent <aphonydissent@hotmail.com>
2013-04-17 22:37:13 -0600

    fix names

--- mods/timedoutput/to_naming.java
+++ mods/timedoutput/to_naming.java
@@ -9,22 +9,22 @@ public class to_naming {
 	
 	public static void init()
     { 
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button5Ticks), "5 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button10Ticks), "10 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button15Ticks), "15 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button20Ticks), "20 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button25Ticks), "25 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button30Ticks), "30 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button35Ticks), "35 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button40Ticks), "40 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button45Ticks), "45 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button50Ticks), "50 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button55Ticks), "55 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button60Ticks), "60 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button65Ticks), "65 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button70Ticks), "70 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button75Ticks), "75 Tick Signal Button");
-        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button80Ticks), "80 Tick Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button5Ticks), "1/4 Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button10Ticks), "1/2 Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button15Ticks), "3/4 Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button20Ticks), "One Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button40Ticks), "Two Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button60Ticks), "Three Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button80Ticks), "Four Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button100Ticks), "Five Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button200Ticks), "Ten Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button300Ticks), "Fifteen Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button400Ticks), "Twenty Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button500Ticks), "Twenty-Five Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button600Ticks), "Thirty Second Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button1200Ticks), "One Minute Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button6000Ticks), "Five Minute Signal Button");
+        LanguageRegistry.instance().addName(new ItemStack(TimedOutput.Button36000Ticks), "Thirty Minute Signal Button");
 
     }
 		
